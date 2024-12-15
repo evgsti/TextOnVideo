72dfbbafc64b3fdc3cec84c3b2cd2c7c4ca417c3
Evgeniy <dev.shipitsyn@yandex.ru>
2024-12-15 10:54:43 +0300

    Initial Commit

---
+++ TextOnVideo/TextOnVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextOnVideoApp.swift
+//  TextOnVideo
+//
+//  Created by Евгений on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TextOnVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
